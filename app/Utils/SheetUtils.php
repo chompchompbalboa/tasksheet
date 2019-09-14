96f07f821b5424f839affd8d65483cca8013e62e
Rocky Eastman Jr <eastmanrjr@gmail.com>
2019-09-14 10:56:31 -0700

    Improved Purchasing Demo seed

--- app/Utils/SheetUtils.php
+++ app/Utils/SheetUtils.php
@@ -5,7 +5,7 @@ namespace App\Utils;
 class SheetUtils
 {
   public static function getColumnType($value) {
-    if (is_bool($value)) return "BOOLEAN";
+    if (is_bool($value) || $value === 'TRUE' || $value === 'FALSE') return "BOOLEAN";
     if (is_numeric(str_replace('%', '', $value))) return "NUMBER";
     return "STRING";
   }
