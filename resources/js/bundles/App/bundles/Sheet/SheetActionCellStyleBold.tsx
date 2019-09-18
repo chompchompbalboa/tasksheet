3c73b83a6817796b0eb8502bdadaa4bf7c5bf78f
eastmanrjr <eastmanrjr@gmail.com>
2019-09-18 17:19:22 -0400

    Updated cell style to use new range selection

--- resources/js/bundles/App/bundles/Sheet/SheetActionCellStyleBold.tsx
+++ resources/js/bundles/App/bundles/Sheet/SheetActionCellStyleBold.tsx
@@ -1,9 +1,8 @@
 //-----------------------------------------------------------------------------
 // Imports
 //-----------------------------------------------------------------------------
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import styled from 'styled-components'
 
 import { BOLD } from '@app/assets/icons'
 
@@ -13,7 +12,7 @@ import {
   updateSheet
 } from '@app/state/sheet/actions'
 
-import Icon from '@/components/Icon'
+import SheetActionCellStyleButton from '@app/bundles/Sheet/SheetActionCellStyleButton'
 
 //-----------------------------------------------------------------------------
 // Component
@@ -24,70 +23,21 @@ const SheetActionCellStyleBold = ({
   
   const dispatch = useDispatch()
   
-  const userColorPrimary = useSelector((state: AppState) => state.user.color.primary)
-  const rows = useSelector((state: AppState) => state.sheet.rows)
-  const selections = useSelector((state: AppState) => state.sheet.sheets && state.sheet.sheets[sheetId] && state.sheet.sheets[sheetId].selections)
-  const sheetStylesBold = useSelector((state: AppState) => state.sheet.sheets && state.sheet.sheets[sheetId] && state.sheet.sheets[sheetId].styles.BOLD)
-  const sheetVisibleColumns = useSelector((state: AppState) => state.sheet.sheets && state.sheet.sheets[sheetId] && state.sheet.sheets[sheetId].visibleColumns)
-  const sheetVisibleRows = useSelector((state: AppState) => state.sheet.sheets && state.sheet.sheets[sheetId] && state.sheet.sheets[sheetId].visibleRows)
+  const sheetStyles = useSelector((state: AppState) => state.sheet.sheets && state.sheet.sheets[sheetId] && state.sheet.sheets[sheetId].styles)
   
-  const [ localSelections, setLocalSelections ] = useState(selections)
-  useEffect(() => {
-    if(selections && selections.rangeStartCellId) {
-      setLocalSelections(selections)
-    }
-  }, [ selections ])
-
-  const handleContainerClick = () => {
-    const {
-      rangeStartCellId,
-      rangeStartColumnId,
-      rangeStartRowId,
-      rangeEndCellId,
-      rangeEndColumnId,
-      rangeEndRowId,
-    } = localSelections
-
-    // Range
-    if(rangeEndCellId) {
-      const isNextStyleBold = !sheetStylesBold.has(rangeStartCellId)
-      const rangeStartColumnIndex = sheetVisibleColumns.findIndex(visibleColumnId => visibleColumnId === rangeStartColumnId)
-      const rangeStartRowIndex = sheetVisibleRows.findIndex(visibleRowId => visibleRowId === rangeStartRowId)
-      const rangeEndColumnIndex = sheetVisibleColumns.findIndex(visibleColumnId => visibleColumnId === rangeEndColumnId)
-      const rangeEndRowIndex = sheetVisibleRows.findIndex(visibleRowId => visibleRowId === rangeEndRowId)
-      const nextSheetStylesBold = new Set([ ...sheetStylesBold ])
-
-      for(let rowIndex = rangeStartRowIndex; rowIndex <= rangeEndRowIndex; rowIndex++) {
-        const rowId = sheetVisibleRows[rowIndex]
-        if(rowId !== 'ROW_BREAK') {
-          const row = rows[rowId]
-          for(let columnIndex = rangeStartColumnIndex; columnIndex <= rangeEndColumnIndex; columnIndex++) {
-            const columnId = sheetVisibleColumns[columnIndex]
-            if(columnId !== 'COLUMN_BREAK') {
-              const cellId = row.cells[columnId]
-              isNextStyleBold ? nextSheetStylesBold.add(cellId) : nextSheetStylesBold.delete(cellId)
-            }
-          }
-        }
-      }
-      dispatch(updateSheet(sheetId, { styles: { BOLD: nextSheetStylesBold }}, true))
-    }
-    // Cell
-    else if(rangeStartCellId) {
-      const isNextStyleBold = !sheetStylesBold.has(rangeStartCellId)
-      const nextSheetStylesBold = new Set([ ...sheetStylesBold ])
-      isNextStyleBold ? nextSheetStylesBold.add(rangeStartCellId) : nextSheetStylesBold.delete(rangeStartCellId)
-      dispatch(updateSheet(sheetId, { styles: { BOLD: nextSheetStylesBold }}, true))
-    }
+  const updateSheetStylesSet = (nextSheetStylesSet: Set<string>) => {
+    dispatch(updateSheet(sheetId, { styles: {
+      ...sheetStyles,
+      BOLD: nextSheetStylesSet 
+    }}))
   }
 
   return (
-    <Container
-      containerBackgroundColor={userColorPrimary}
-      onClick={handleContainerClick}>
-      <Icon 
-        icon={BOLD}/>
-    </Container>
+    <SheetActionCellStyleButton
+      sheetId={sheetId}
+      icon={BOLD}
+      sheetStylesSet={sheetStyles && sheetStyles.BOLD}
+      updateSheetStylesSet={updateSheetStylesSet}/>
   )
 }
 
@@ -96,29 +46,6 @@ const SheetActionCellStyleBold = ({
 //-----------------------------------------------------------------------------
 interface SheetActionCellStyleBoldProps {
   sheetId: Sheet['id']
-}
-
-//-----------------------------------------------------------------------------
-// Styled Components
-//-----------------------------------------------------------------------------
-const Container = styled.div`
-  cursor: pointer;  
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgb(210, 210, 210);
-  color: rgb(80, 80, 80);
-  text-decoration: none;
-  border-radius: 3px;
-  padding: 0.4rem;
-  transition: all 0.05s;
-  &:hover {
-    background-color: ${ ({ containerBackgroundColor }: IContainer) => containerBackgroundColor};
-    color: rgb(240, 240, 240);
-  }
-`
-interface IContainer {
-  containerBackgroundColor: string
 }
 
 //-----------------------------------------------------------------------------
---
+++ resources/js/bundles/App/bundles/Sheet/SheetActionCellStyleButton.tsx
@@ -0,0 +1,118 @@
+//-----------------------------------------------------------------------------
+// Imports
+//-----------------------------------------------------------------------------
+import React from 'react'
+import { useSelector } from 'react-redux'
+import styled from 'styled-components'
+
+import { AppState } from '@app/state'
+import { Sheet } from '@app/state/sheet/types'
+
+import Icon from '@/components/Icon'
+
+//-----------------------------------------------------------------------------
+// Component
+//-----------------------------------------------------------------------------
+const SheetActionCellStyleButton = ({
+  sheetId,
+  icon,
+  sheetStylesSet,
+  updateSheetStylesSet
+}: SheetActionCellStyleButtonProps) => {
+  
+  const userColorPrimary = useSelector((state: AppState) => state.user.color.primary)
+  const rows = useSelector((state: AppState) => state.sheet.rows)
+  const selections = useSelector((state: AppState) => state.sheet.sheets && state.sheet.sheets[sheetId] && state.sheet.sheets[sheetId].selections)
+  const sheetVisibleColumns = useSelector((state: AppState) => state.sheet.sheets && state.sheet.sheets[sheetId] && state.sheet.sheets[sheetId].visibleColumns)
+  const sheetVisibleRows = useSelector((state: AppState) => state.sheet.sheets && state.sheet.sheets[sheetId] && state.sheet.sheets[sheetId].visibleRows)
+  
+  const addOrDeleteFromSet = sheetStylesSet && sheetStylesSet.has(selections.rangeStartCellId) ? 'DELETE' : 'ADD'
+
+  const handleContainerClick = () => {
+    const {
+      rangeStartCellId,
+      rangeStartColumnId,
+      rangeStartRowId,
+      rangeEndCellId,
+      rangeEndColumnId,
+      rangeEndRowId,
+    } = selections
+
+    // Range
+    if(rangeEndCellId) {
+      const rangeStartColumnIndex = sheetVisibleColumns.findIndex(visibleColumnId => visibleColumnId === rangeStartColumnId)
+      const rangeStartRowIndex = sheetVisibleRows.findIndex(visibleRowId => visibleRowId === rangeStartRowId)
+      const rangeEndColumnIndex = sheetVisibleColumns.findIndex(visibleColumnId => visibleColumnId === rangeEndColumnId)
+      const rangeEndRowIndex = sheetVisibleRows.findIndex(visibleRowId => visibleRowId === rangeEndRowId)
+      const nextSheetStylesSet = new Set([ ...sheetStylesSet ])
+
+      for(let rowIndex = rangeStartRowIndex; rowIndex <= rangeEndRowIndex; rowIndex++) {
+        const rowId = sheetVisibleRows[rowIndex]
+        if(rowId !== 'ROW_BREAK') {
+          const row = rows[rowId]
+          for(let columnIndex = rangeStartColumnIndex; columnIndex <= rangeEndColumnIndex; columnIndex++) {
+            const columnId = sheetVisibleColumns[columnIndex]
+            if(columnId !== 'COLUMN_BREAK') {
+              const cellId = row.cells[columnId]
+              addOrDeleteFromSet === 'ADD' ? nextSheetStylesSet.add(cellId) : nextSheetStylesSet.delete(cellId)
+            }
+          }
+        }
+      }
+      updateSheetStylesSet(nextSheetStylesSet)
+    }
+    // Cell
+    else if(rangeStartCellId) {
+      const nextSheetStylesSet = new Set([ ...sheetStylesSet ])
+      addOrDeleteFromSet === 'ADD' ? nextSheetStylesSet.add(rangeStartCellId) : nextSheetStylesSet.delete(rangeStartCellId)
+      updateSheetStylesSet(nextSheetStylesSet)
+    }
+  }
+
+  return (
+    <Container
+      containerBackgroundColor={userColorPrimary}
+      onClick={handleContainerClick}>
+      <Icon 
+        icon={icon}/>
+    </Container>
+  )
+}
+
+//-----------------------------------------------------------------------------
+// Props
+//-----------------------------------------------------------------------------
+interface SheetActionCellStyleButtonProps {
+  sheetId: Sheet['id']
+  icon: string
+  sheetStylesSet: Set<string>
+  updateSheetStylesSet(nextSheetStylesSet: Set<string>): void
+}
+
+//-----------------------------------------------------------------------------
+// Styled Components
+//-----------------------------------------------------------------------------
+const Container = styled.div`
+  cursor: pointer;  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(210, 210, 210);
+  color: rgb(80, 80, 80);
+  text-decoration: none;
+  border-radius: 3px;
+  padding: 0.4rem;
+  transition: all 0.05s;
+  &:hover {
+    background-color: ${ ({ containerBackgroundColor }: IContainer) => containerBackgroundColor};
+    color: rgb(240, 240, 240);
+  }
+`
+interface IContainer {
+  containerBackgroundColor: string
+}
+
+//-----------------------------------------------------------------------------
+// Export
+//-----------------------------------------------------------------------------
+export default SheetActionCellStyleButton
