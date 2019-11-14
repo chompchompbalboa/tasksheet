cbcfe2483512f574ed0fc79a61c7c6a20fb055f3
eastmanrjr <eastmanrjr@gmail.com>
2019-11-14 18:59:45 -0500

    Fix typos

--- resources/js/bundles/Site/SiteFeatures.tsx
+++ resources/js/bundles/Site/SiteFeatures.tsx
@@ -28,7 +28,7 @@ const SiteFeatures = ({
         <SiteFeaturesList
           header="A format you're already familiar with...">
           <SiteFeaturesListItem>
-            Anyone familiar with Microsoft Excel, Google Sheets or any other spreadsheet app can jump right in to using Tasksheet - no training required.
+            Anyone familiar with Microsoft Excel, Google Sheets or any other spreadsheet app can jump right in to using Tasksheet - with little or no training needed.
           </SiteFeaturesListItem>
           <SiteFeaturesListItem>
             <SiteFeaturesActionButton
@@ -45,12 +45,12 @@ const SiteFeatures = ({
           <SiteFeaturesListItem>
             Save photos and files. Take notes and track changes to your sheet over time. Add dates and create Gantt charts. Save custom views for quick access later. And much, much more.
             <br/><br/>
-            We've built Tasksheet and its features with the specific purpose of saving you time and money.
+            We've built Tasksheet and its features to be quick and easy to use, so you can spend your time focusing on doing your work, not organizing your work.
           </SiteFeaturesListItem>
           <SiteFeaturesListItem>
             <SiteFeaturesActionButton
               onClick={() => handleActionButtonClick()}
-              text="I like saving money"/>
+              text="Check it out"/>
           </SiteFeaturesListItem>
         </SiteFeaturesList>
       </SiteFeaturesFeature>
@@ -59,12 +59,12 @@ const SiteFeatures = ({
         <SiteFeaturesList
           header="Empower your team">
           <SiteFeaturesListItem>
-            With the ability to save custom views for quick access, every member of your team can see the exact level of detail the need - no matter their role or their current task at hand.
+            With the ability to save custom views for quick access, every member of your team can see the exact level of detail they need - no matter their role or their current task at hand.
           </SiteFeaturesListItem>
           <SiteFeaturesListItem>
             <SiteFeaturesActionButton
               onClick={() => handleActionButtonClick()}
-              text="Try it out"/>
+              text="Sign up now"/>
           </SiteFeaturesListItem>
         </SiteFeaturesList>
       </SiteFeaturesFeature>
@@ -75,7 +75,7 @@ const SiteFeatures = ({
         <SiteFeaturesList
           header="Endlessly customizable">
           <SiteFeaturesListItem>
-            Don't modify your existing workflow to fit your task management app. Instead, build your Tasksheets to fit your needs. With an exhaustive library of cell types and powerful customization controls over all of them, Tasksheet can be molded to fit any workflow.
+            Don't modify your existing workflow to fit your task management app. Instead, Tasksheet is designed to fit your needs, no matter what they are. With an exhaustive library of cell types and powerful customization built into all of them, Tasksheet can be molded to fit any workflow.
           </SiteFeaturesListItem>
           <SiteFeaturesListItem>
             <SiteFeaturesActionButton
@@ -89,7 +89,7 @@ const SiteFeatures = ({
         <SiteFeaturesList
           header="More than just tasks">
           <SiteFeaturesListItem>
-            We built Tasksheet with tasks in mind but we think it's pretty darn useful for just about anything else as well. We've seen it used to manage inventory, track files through approval processes, dig through analytics and much, muchm more.
+            We built Tasksheet with tasks in mind, but we think it's pretty darn useful for just about anything else as well. We've seen it used to manage inventory, track files through approval processes, review analytics and much, much more.
           </SiteFeaturesListItem>
           <SiteFeaturesListItem>
             <SiteFeaturesActionButton
