1a5f89b32d3a239a1d03210bc8952f2ce51f794a
Rocky Eastman Jr <eastmanrjr@gmail.com>
2019-12-17 21:30:09 -0800

    [UI] Minor change to see if we can fix jumpiness in iOS Safari

--- resources/js/bundles/App/bundles/Sheet/SheetGrid.tsx
+++ resources/js/bundles/App/bundles/Sheet/SheetGrid.tsx
@@ -138,6 +138,7 @@ const GridItems = styled.div`
   width: 100%;
   height: 100%;
   position: relative;
+  -webkit-overflow-scrolling: touch;
 `
 
 //-----------------------------------------------------------------------------
