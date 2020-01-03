bc67c8f54a2d87cd21ab50da9faa02523b892d0e
Rocky Eastman Jr <eastmanrjr@gmail.com>
2020-01-02 21:22:27 -0800

    [Desktop][Bug] Convert rem sizes in Icon to px to make all the browsers happy

--- resources/js/components/Icon.tsx
+++ resources/js/components/Icon.tsx
@@ -9,16 +9,32 @@ import React from 'react'
 const Icon = ({ 
   icon, 
   size = '1rem'
-}: IconProps) => (
-  <svg
-    width={`${size}`}
-    height={`${size}`}
-    viewBox="0 0 1024 1024">
-    <path
-      d={icon}
-      style={{ fill: "currentColor" }} />
-  </svg>
-)
+}: IconProps) => {
+
+  // Convert rem to pixels (rem is not supported as an svg size unit in all
+  // browsers, but being able to use rem's is quite helpful while developing)
+  const pxSize = size.toLowerCase().includes('rem')
+    ? convertRemToPixels(Number(size.replace('rem', '')))
+    : size
+
+  return (
+    <svg
+      width={`${pxSize}`}
+      height={`${pxSize}`}
+      viewBox="0 0 1024 1024">
+      <path
+        d={icon}
+        style={{ fill: "currentColor" }} />
+    </svg>
+  )
+}
+
+//------------------------------------------------------------------------------
+// Convert rem to px
+//------------------------------------------------------------------------------
+const convertRemToPixels = (rem: number) => {    
+  return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
 
 //------------------------------------------------------------------------------
 // Props
