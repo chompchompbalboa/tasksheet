95538cd0645bfc4c996bcd113eb616f0cd56045e
Rocky Eastman Jr <eastmanrjr@gmail.com>
2020-02-19 21:12:58 -0800

    [Tests] SettingsUserName

--- resources/js/bundles/Desktop/Settings/__tests__/SettingsUserName.test.tsx
+++ resources/js/bundles/Desktop/Settings/__tests__/SettingsUserName.test.tsx
@@ -6,7 +6,8 @@ import 'jest-styled-components'
 import '@testing-library/jest-dom/extend-expect'
 import axiosMock from 'axios'
 
-import { renderWithRedux } from '@/testing/library'
+import { fireEvent,  renderWithRedux } from '@/testing/library'
+import { flushPromises } from '@/testing/utils'
 import { appStateFactory, IAppStateFactoryInput } from '@/testing/mocks/appState'
 
 import SettingsUserName from '@desktop/Settings/SettingsUserName'
@@ -37,17 +38,52 @@ describe('SettingsUserName', () => {
 
   const settingsUserName = () => {
     const { 
-      getByLabelText
+      getByLabelText,
+      store: {
+        getState
+      }
     } = renderWithRedux(<SettingsUserName />)
     const nameInput = getByLabelText('Name:') as HTMLInputElement
     return {
+      getState,
       nameInput
     }
   }
   
-  it("display a 'Name:' input", async () => {
+  it("displays a 'Name:' input", async () => {
     const { nameInput } = settingsUserName()
     expect(nameInput).toBeTruthy()
+  })
+  
+  it("displays the user's name in the 'Name:' input", async () => {
+    const { getState, nameInput } = settingsUserName()
+    expect(nameInput.value).toBe(getState().user.name)
+  })
+  
+  it("changes the 'Name:' value on user input", async () => {
+    const { nameInput } = settingsUserName()
+    const nextName = 'Next Name'
+    fireEvent.change(nameInput, { target: { value: nextName }})
+    expect(nameInput.value).toBe(nextName)
+  })
+  
+  it("updates the app state when the 'Name:' value changes and the user blurs the input", async () => {
+    const { getState, nameInput } = settingsUserName()
+    const nextName = 'Next Name'
+    expect(getState().user.name).not.toBe(nextName)
+    fireEvent.change(nameInput, { target: { value: nextName }})
+    fireEvent.blur(nameInput)
+    flushPromises()
+    expect(getState().user.name).toBe(nextName)
+  })
+  
+  it("sends the update to the database when the 'Name:' value changes and the user blurs the input", async () => {
+    const { getState, nameInput } = settingsUserName()
+    const nextName = 'Next Name'
+    fireEvent.change(nameInput, { target: { value: nextName }})
+    fireEvent.blur(nameInput)
+    flushPromises()
+    expect(axiosMock.patch).toHaveBeenCalledWith('/app/user/' + getState().user.id, { name: nextName })
   })
 
 })
