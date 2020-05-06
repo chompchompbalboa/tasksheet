d8d05929932e2719540281fcb2df441d50f6fdd4
Rocky Eastman <eastmanrjr@gmail.com>
2020-05-06 07:46:35 -0700

    Local to CA

--- resources/js/bundles/Desktop/Sheet/__tests__/SheetHeaderGanttRanges.test.tsx
+++ resources/js/bundles/Desktop/Sheet/__tests__/SheetHeaderGanttRanges.test.tsx
@@ -2,6 +2,7 @@
 // Imports
 //-----------------------------------------------------------------------------
 import React from 'react'
+import axiosMock from 'axios'
 import 'jest-styled-components'
 
 import { 
@@ -21,7 +22,6 @@ import { IAppState } from '@/state'
 import Messenger from '@desktop/Messenger/Messenger'
 import SheetHeaderGanttRanges, { ISheetHeaderGanttRanges } from '@/bundles/Desktop/Sheet/SheetHeaderGanttRanges'
 
-
 //-----------------------------------------------------------------------------
 // Mocks
 //-----------------------------------------------------------------------------
@@ -35,40 +35,8 @@ const mockAppStateFactoryInput: IMockAppStateFactoryInput = {
     'GANTT'
   ]
 }
-
-const {
-  folder: {
-    allFolders,
-    allFiles
-  },
-  sheet: {
-    allSheets,
-    allSheetGantts,
-    allSheetViews
-  }
-} = getMockAppState(mockAppStateFactoryInput)
-
-const labelsColumnIndex = mockAppStateColumnTypes.findIndex(columnType => columnType === 'GANTT')
-const sheetGanttId = allSheetGantts[Object.keys(allSheetGantts)[0]].id
-
-const folderId = Object.keys(allFolders)[0]
-const fileId = allFolders[folderId].files[0]
-const file = allFiles[fileId]
-const sheetId = file.typeId
-const sheet = allSheets[sheetId]
-const activeSheetView = allSheetViews[sheet.activeSheetViewId]
-const sheetColumnId = activeSheetView.visibleColumns[labelsColumnIndex]
-
+    
 console.warn = jest.fn()
-
-//-----------------------------------------------------------------------------
-// Props
-//-----------------------------------------------------------------------------
-const props: ISheetHeaderGanttRanges = {
-  sheetId: sheetId,
-  columnId: sheetColumnId,
-  sheetGanttId
-}
 
 //-----------------------------------------------------------------------------
 // Tests
@@ -80,6 +48,39 @@ describe('SheetHeaderGanttRanges', () => {
   })
 
   const sheetHeaderGanttRanges = (appState: IAppState = getMockAppState(mockAppStateFactoryInput)) => {
+    
+    const {
+      folder: {
+        allFolders,
+        allFiles
+      },
+      sheet: {
+        allSheets,
+        allSheetColumns,
+        allSheetGantts,
+        allSheetViews,
+      }
+    } = appState
+    
+    const ganttColumnIndex = mockAppStateColumnTypes.findIndex(columnType => columnType === 'GANTT')
+    const sheetGanttId = allSheetGantts[Object.keys(allSheetGantts)[0]].id
+    
+    const folderId = Object.keys(allFolders)[0]
+    const fileId = allFolders[folderId].files[0]
+    const file = allFiles[fileId]
+    const sheetId = file.typeId
+    const sheet = allSheets[sheetId]
+    const activeSheetView = allSheetViews[sheet.activeSheetViewId]
+    const sheetColumnId = activeSheetView.visibleColumns[ganttColumnIndex]
+    const dateColumn1Id = activeSheetView.visibleColumns[0]
+    const dateColumn2Id = activeSheetView.visibleColumns[1]
+
+    const props: ISheetHeaderGanttRanges = {
+      sheetId: sheetId,
+      columnId: sheetColumnId,
+      sheetGanttId
+    }
+
     const {
       getByTestId,
       getByText,
@@ -97,9 +98,13 @@ describe('SheetHeaderGanttRanges', () => {
     const dropdownButton = getByTestId('SheetHeaderGanttRangesDropdownButton')
     const startColumn = within(dropdown).getByText("Start Column")
     const endColumn = within(dropdown).getByText("End Column (Optional)")
+    const startColumnDateColumn1 = within(dropdown).getAllByText(allSheetColumns[dateColumn1Id].name)[0]
+    const endColumnDateColumn2 = within(dropdown).getAllByText(allSheetColumns[dateColumn2Id].name)[1]
     const createGanttRangeButton = within(dropdown).getByTestId("SheetHeaderGanttRangesCreateRangeButton")
     return {
       createGanttRangeButton,
+      startColumnDateColumn1, 
+      endColumnDateColumn2, 
       dropdown,
       dropdownButton,
       endColumn,
@@ -125,9 +130,29 @@ describe('SheetHeaderGanttRanges', () => {
   })
 
   it("correctly creates a new gantt range with only a start date", async () => {
-    const { createGanttRangeButton, startColumn, endColumn } = sheetHeaderGanttRanges()
-    expect(startColumn).toBeTruthy()
-    expect(endColumn).toBeTruthy()
-    expect(createGanttRangeButton).toBeTruthy()
+    (axiosMock.post as jest.Mock).mockResolvedValueOnce({})
+    const { createGanttRangeButton, getState, startColumnDateColumn1 } = sheetHeaderGanttRanges()
+    expect(getState().sheet.allSheetGanttRanges).toBeNull()
+    fireEvent.click(startColumnDateColumn1)
+    fireEvent.click(createGanttRangeButton)
+    expect(getState().sheet.allSheetGanttRanges).not.toBeNull()
+    expect(Object.keys(getState().sheet.allSheetGanttRanges).length).toBe(1)
+    expect(axiosMock.post).toHaveBeenCalledTimes(1)
+    expect(getState().sheet.allSheetGanttRanges[Object.keys(getState().sheet.allSheetGanttRanges)[0]].startDateColumnId).not.toBeNull()
+    expect(getState().sheet.allSheetGanttRanges[Object.keys(getState().sheet.allSheetGanttRanges)[0]].endDateColumnId).toBeNull()
+  })
+
+  it("correctly creates a new gantt range with a start date and an end date", async () => {
+    (axiosMock.post as jest.Mock).mockResolvedValueOnce({})
+    const { createGanttRangeButton, endColumnDateColumn2, getState, startColumnDateColumn1 } = sheetHeaderGanttRanges()
+    expect(getState().sheet.allSheetGanttRanges).toBeNull()
+    fireEvent.click(startColumnDateColumn1)
+    fireEvent.click(endColumnDateColumn2)
+    fireEvent.click(createGanttRangeButton)
+    expect(getState().sheet.allSheetGanttRanges).not.toBeNull()
+    expect(Object.keys(getState().sheet.allSheetGanttRanges).length).toBe(1)
+    expect(axiosMock.post).toHaveBeenCalledTimes(1)
+    expect(getState().sheet.allSheetGanttRanges[Object.keys(getState().sheet.allSheetGanttRanges)[0]].startDateColumnId).not.toBeNull()
+    expect(getState().sheet.allSheetGanttRanges[Object.keys(getState().sheet.allSheetGanttRanges)[0]].endDateColumnId).not.toBeNull()
   })
 })
